f8d1be235e24766ed134b057e715974c87064519
BossNumber1 <dimaang23@gmail.com>
2021-06-26 19:04:04 +0300

    добавил файлы

---
+++ Zaykina_Toys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Zaykina_Toys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Zaykina_Toys.settings')
+
+application = get_asgi_application()
